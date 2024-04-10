aec87e742f76c49d7afaea66c1b2cedd9aa15d25
Artur Alonso <166646443+arturfernandess@users.noreply.github.com>
2024-04-10 17:04:03 -0300

    Create Calculadora.java

---
+++ Calculadora.java
@@ -0,0 +1,21 @@
+package listaUm;
+
+public class Calculadora {
+	public int somar (int x, int y) {
+		return (x + y);
+	}
+	
+	public int subtrair (int x, int y){
+		return (x - y);
+	}
+	
+	public int multipilcar (int x, int y) {
+		return (x * y);
+		
+	}
+	
+	public int dividir (int x, int y) {
+		return (x / y);
+	}
+		
+}
