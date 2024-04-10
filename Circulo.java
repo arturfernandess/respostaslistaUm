1ba36735358f0d3db70c9645c36a6f06ac501e30
Artur Alonso <166646443+arturfernandess@users.noreply.github.com>
2024-04-10 17:05:38 -0300

    Create Circulo.java

---
+++ Circulo.java
@@ -0,0 +1,28 @@
+package listaUm;
+
+public class Circulo {
+	private double raio;
+	
+	public Circulo (double r) {
+		this.raio = r;
+				
+	}
+	
+	public double getRaio () {
+		return raio;
+	}
+	
+	public void setRaio (double r) {
+		this.raio = r;
+	}
+	
+	public double CalcularArea () {
+		return 3.14 * (this.raio * this.raio);
+		
+	}
+	
+	public double CalcularPerimetro () {
+		return (2 * 3.14 * this.raio) ;
+	}
+	
+}
