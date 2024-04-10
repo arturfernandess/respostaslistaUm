a80e606fefb796e7afcddc300ad4870ab32fc82f
Artur Alonso <166646443+arturfernandess@users.noreply.github.com>
2024-04-10 17:08:07 -0300

    Create Animal.java

---
+++ Animal.java
@@ -0,0 +1,34 @@
+package listaUm;
+
+public class Animal {
+	private String nome;
+	private String tipo;
+	private boolean emitirSom;
+	
+	public Animal (String n, String t) {
+		this.nome = n;
+		this.tipo = t;
+	}
+	
+	public String getNome () {
+		return nome;
+	}
+	
+	public String getTipo () {
+		return tipo;
+	}
+	
+	public void setNome (String n) {
+		this.nome = n;
+	}
+	
+	public void setTipo (String t) {
+		this.tipo = t;
+		
+	}
+	
+	public void emitirSom () {
+		this.emitirSom = true; 
+	}
+
+}
