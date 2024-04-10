ab9678c3cd99b1d1b81c2123161ee49f598d667f
Artur Alonso <166646443+arturfernandess@users.noreply.github.com>
2024-04-10 17:19:13 -0300

    Create DataHora.java

---
+++ DataHora.java
@@ -0,0 +1,35 @@
+package listaUm;
+
+public class DataHora {
+	Data data;
+	Hora hora;
+	
+	public DataHora (Data d, Hora h) {
+		this.data = d;
+		this.hora = h;
+	}
+	
+	public Data getData () {
+		return data;
+		
+	}
+	
+	public Hora getHora() {
+		return hora;
+		
+	}
+	
+	public void setData(Data novaData) {
+		this.data = novaData;
+		
+	}
+	
+	public void setHora (Hora novaHora) {
+		this.hora = novaHora;
+	}
+	
+	public String imprimirDataHora() {
+		return "hora: " + hora.imprimirHora() + "\nData: " + data.imprimirData();
+	}
+
+}
